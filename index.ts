8c36d326bd5c37d40afe18bbf3cf0449ed778fff
proto-taro <morita.naoya@prsys.jp>
2025-10-22 10:18:22 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
 serve(async (req) => {
-  const response = await fetch("https://xxxx.supabase.co/rest/v1/prsys_weather_feedback", {
+  const response = await fetch("https://uzyfrplsptljlhqjayff.supabase.co/rest/v1/prsys_weather_feedback", {
     method: "GET",
     headers: {
-      "apikey": "your-anon-key",
-      "Authorization": "Bearer your-anon-key",
+      "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6eWZycGxzcHRsamxocWpheWZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5Mzc5ODIsImV4cCI6MjA3NjUxMzk4Mn0.ysm-pWfEo-G9f_LhnbJf4JgIuGPm7jotc0G44L3sMCc",
+      "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6eWZycGxzcHRsamxocWpheWZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5Mzc5ODIsImV4cCI6MjA3NjUxMzk4Mn0.ysm-pWfEo-G9f_LhnbJf4JgIuGPm7jotc0G44L3sMCc",
       "Content-Type": "application/json"
     }
   });
